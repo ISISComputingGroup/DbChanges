4cbc9708b3415ae8db2581cfd8cfe7bd45ef577f
Lowri Jenkins <lowri.jenkins@stfc.ac.uk>
2024-05-17 14:41:49 +0100

    Remove all reference to six

--- src/db_parser/parser.py
+++ src/db_parser/parser.py
@@ -1,4 +1,3 @@
-import six
 from contextlib import contextmanager
 
 from src.db_parser.tokens import TokenTypes
@@ -20,7 +19,7 @@ class Parser(object):
         """
         try:
             # Ignore macros and comments wherever they occur
-            self.current_token = six.advance_iterator(self.lexer)
+            self.current_token = next(self.lexer)
         except StopIteration:
             self.raise_error("Next token was requested, but none exists.")
 
